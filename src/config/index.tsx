c7cf0107617fd0d116bf8eb1bdad9ace7cc2fe8e
Adebayo Muis <charmingdc002@gmail.com>
2025-03-09 09:50:05 +0100

    :)

---
+++ src/config/index.tsx
@@ -0,0 +1,24 @@
+import { mainnet, arbitrum, sepolia } from '@reown/appkit/networks'
+import type { AppKitNetwork } from '@reown/appkit/networks'
+import { EthersAdapter } from '@reown/appkit-adapter-ethers'
+
+// Get projectId from https://cloud.reown.com
+export const projectId = "aa6d5c4004aadcda473fa771248d2305" // this is a public projectId only to use on localhost
+
+if (!projectId) {
+  throw new Error('Project ID is not defined')
+}
+
+// Create a metadata object - optional
+export const metadata = {
+  name: 'AppKit',
+  description: 'AppKit Example',
+  url: 'http://localhost:5173/', // origin must match your domain & subdomain
+  icons: ['https://avatars.githubusercontent.com/u/179229932']
+}
+
+// for custom networks visit -> https://docs.reown.com/appkit/react/core/custom-networks
+export const networks = [mainnet, arbitrum, sepolia] as [AppKitNetwork, ...AppKitNetwork[]]
+
+// Set up Solana Adapter
+export const ethersAdapter = new EthersAdapter();
